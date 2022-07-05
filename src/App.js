be24d578086c012e5731d6d28baf6515e66fd3a5
witzkvn <witz.kevin05@gmail.com>
2022-07-05 14:01:18 +0200

    project global structure with title and box component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import "./App.css";
+import styles from "./app.module.css";
+import BlockWrapper from "./layout/block-wrapper/BlockWrapper";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <main className={styles.app}>
+      <h1 className={styles.title}>
+        SPLI
+        <br />
+        TTER
+      </h1>
+      <BlockWrapper
+        className={styles.wrapper}
+        level="primary"
+        withShadow={true}
+      >
+        <BlockWrapper level="primary" withShadow={false}>
+          left
+        </BlockWrapper>
+        <BlockWrapper level="secondary" withShadow={false}>
+          right
+        </BlockWrapper>
+      </BlockWrapper>
+    </main>
+  );
 }
 
 export default App;
